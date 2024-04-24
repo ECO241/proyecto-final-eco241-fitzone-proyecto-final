fe453a19c3a9f7a15864af2a551a30535357eebe
LOPZU <116040988+GLopzu@users.noreply.github.com>
2024-04-24 18:44:06 -0500

    Login

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Obtener el nombre de usuario almacenado en localStorage
-  const username = localStorage.getItem('username');
-
-  // Verificar si el nombre de usuario existe y actualizar el saludo
-  if (username) {
-      const greetingElement = document.getElementById('greeting');
-      greetingElement.textContent = `Hello, ${username}`;
-  }
-});
-
+    console.log('Script de home cargado.');
+  
+    // Aquí se va agregando lo siguiente
+  });
+  
