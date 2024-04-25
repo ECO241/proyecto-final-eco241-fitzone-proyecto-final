68965ab798243e5c08dc1ed86560da3d80da05ad
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2024-04-25 16:01:34 -0500

    css

--- src/pages/settingsProf/setting.js
+++ src/pages/settingsProf/setting.js
@@ -1,22 +1,34 @@
 class SettingProfile extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-    }
-  
-    connectedCallback() {
-      this.render();
-    }
-  
-    render() {
-      this.shadowRoot.innerHTML = `
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
               <head>
                   <link rel="stylesheet" type="text/css" href="./setting.css">
               </head>
-              <p>hola</p>
+              <img src="../../imgs/left arrow.png">
+              <h1>Settings</h1>
+
+              <h2>Account</h2>
+              <div class="container">
+    <div class="photo">
+        <img src="../../imgs/profilePicc.png" class="photo">
+    </div>
+    <div class="info">
+        <h3>Maria Lupe</h3>
+        <p>Personal info</p>
+    </div>
+</div>
+
              `;
-    }
   }
-  
-  customElements.define("setting-profile", SettingProfile);
-  
+}
+
+customElements.define("setting-profile", SettingProfile);
