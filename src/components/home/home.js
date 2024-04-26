d64702f6da1fdb2f09cac59959eded6e24410c88
David <114586818+ZDavid73@users.noreply.github.com>
2024-04-25 23:11:31 -0500

    David rama

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Script de home cargado.');
-  
-    // Aquí se va agregando lo siguiente
-  });
-  
+  // Obtener el nombre de usuario almacenado en localStorage
+  const username = localStorage.getItem('username');
+
+  // Verificar si el nombre de usuario existe y actualizar el saludo
+  if (username) {
+      const greetingElement = document.getElementById('greeting');
+      greetingElement.textContent = `Hello, ${username}`;
+  }
+});
+
