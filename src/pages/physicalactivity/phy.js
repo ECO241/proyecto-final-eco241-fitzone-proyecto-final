534a9fd8f2e09559a5d598cb4f1fd8dab09c75e3
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2024-05-16 14:45:09 -0500

    dinamic object

--- src/pages/physicalactivity/phy.js
+++ src/pages/physicalactivity/phy.js
@@ -1,52 +1,61 @@
 class PhysicalAct extends HTMLElement {
   constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
+      super();
+      this.attachShadow({ mode: "open" });
+      this.activityLevels = [
+          { id: 'container1', text: 'Beginner' },
+          { id: 'container2', text: 'Intermediate' },
+          { id: 'container3', text: 'Advanced' },
+          { id: 'container4', text: 'Expert' }
+      ];
   }
 
   connectedCallback() {
-    this.render();
+      this.render();
+  }
+
+  addActivityLevel(id, text) {
+      this.activityLevels.push({ id, text });
+      this.render();
+  }
+
+  removeActivityLevel(id) {
+      this.activityLevels = this.activityLevels.filter(level => level.id !== id);
+      this.render();
+  }
+
+  toggleCheckbox(id) {
+      const checkbox = this.shadowRoot.querySelector(`#${id}`);
+      if (checkbox) {
+          checkbox.checked = !checkbox.checked;
+      }
   }
 
   render() {
-    this.shadowRoot.innerHTML = `
-            <head>
-                <link rel="stylesheet" type="text/css" href="./phy.css">
-            </head>
-            <img src="../../imgs/left arrow.png">
-            <h1>What is Your level of physical activity?</h1>
-            <center>
-                <p>Select your current level of physical activity to provide 
-                you with suitable recommendations to achieve your fitness goals.</p>
-            </center>
+      const activityHtml = this.activityLevels.map(level => `
+          <label for="${level.id}">
+              <div class="container">${level.text}</div>
+              <input type="checkbox" id="${level.id}" class="checkbox">
+          </label>
+      `).join('');
 
-
-            <center>
-
-            <div class="options">
-            <label for="container1">
-  <div class="container">Beginner</div>
-  <input type="checkbox" id="container1" class="checkbox">
-</label>
-
-<label for="container2">
-  <div class="container">Intermediate</div>
-  <input type="checkbox" id="container2" class="checkbox">
-</label>
-
-<label for="container3">
-  <div class="container">Advanced</div>
-  <input type="checkbox" id="container3" class="checkbox">
-</label>
-
-<label for="container4">
-  <div class="container">Expert</div>
-  <input type="checkbox" id="container4" class="checkbox">
-</label>
-</div>
-<button class="gradient-button" onclick="window.location.href='../completeProf/profilesign.html';">Continue</button>
-</center>
-        `;
+      this.shadowRoot.innerHTML = `
+          <head>
+              <link rel="stylesheet" type="text/css" href="./phy.css">
+          </head>
+          <img src="../../imgs/left arrow.png">
+          <h1>What is Your level of physical activity?</h1>
+          <center>
+              <p>Select your current level of physical activity to provide 
+              you with suitable recommendations to achieve your fitness goals.</p>
+          </center>
+          <center>
+              <div class="options">
+                  ${activityHtml}
+              </div>
+              <button class="gradient-button" onclick="window.location.href='../completeProf/profilesign.html';">Continue</button>
+          </center>
+      `;
   }
 }
 
