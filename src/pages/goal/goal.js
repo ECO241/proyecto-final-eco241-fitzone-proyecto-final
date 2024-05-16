7c5a9e7f8ad0779317dc380226f009677bfa69b4
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2024-05-16 14:40:36 -0500

    dinamic objects

--- src/pages/goal/goal.js
+++ src/pages/goal/goal.js
@@ -1,115 +1,50 @@
 class GoalScreen extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({mode: "open"});
-    }
+  constructor() {
+      super();
+      this.attachShadow({ mode: "open" });
+      this.goals = [
+          { id: 'checkbox', text: 'Lose weight' },
+          { id: 'checkbox1', text: 'Gain muscle mass' },
+          { id: 'checkbox2', text: 'Increase strength' },
+          { id: 'checkbox3', text: 'Enhance flexibility' },
+          { id: 'checkbox4', text: 'Increase endurance' },
+          { id: 'checkbox5', text: 'Reduce the risk of injuries' },
+          { id: 'checkbox6', text: 'Adopt a healthy lifestyle' }
+      ];
+  }
 
-    connectedCallback() {
-        this.render();
-    }
+  connectedCallback() {
+      this.render();
+  }
 
-   render(){
-    this.shadowRoot.innerHTML = `
-    <link rel="stylesheet" type="text/css" href="./goal.css"></head>
-    <img src="../../imgs/left arrow.png">
+  render() {
+      const goalHtml = this.goals.map(goal => `
+          <center>
+              <div class="container">
+                  <div class="text">
+                      <p>${goal.text}</p>
+                  </div>
+                  <div class="round">
+                      <input type="checkbox" checked id="${goal.id}" />
+                      <label for="${goal.id}"></label>
+                  </div>
+              </div>
+          </center>
+      `).join('');
 
-    <h1>What is your goal?</h1>
-    <center> <p>Select your fitness goal to personalize 
-    your experience in FitZone. Let's get started!</p> </center>
-
-
-   <center> <div class="container">
-
-    <div class="text">
-    <p>Lose weight</p>
-    </div>
-
-  <div class="round">
-    <input type="checkbox" checked id="checkbox" />
-    <label for="checkbox"></label>
-  </div>
-</div> </center>
-   
-<center> <div class="container">
-
-    <div class="text">
-    <p>Gain muscle mass</p>
-    </div>
-
-  <div class="round">
-    <input type="checkbox" checked id="checkbox1" />
-    <label for="checkbox1"></label>
-  </div>
-</div> </center>
-
-<center> <div class="container">
-
-    <div class="text">
-    <p>Increase strength</p>
-    </div>
-
-  <div class="round">
-    <input type="checkbox" checked id="checkbox2" />
-    <label for="checkbox2"></label>
-  </div>
-</div> </center>
-
-<center> <div class="container">
-
-    <div class="text">
-    <p>Enhance flexibility</p>
-    </div>
-
-  <div class="round">
-    <input type="checkbox" checked id="checkbox3" />
-    <label for="checkbox3"></label>
-  </div>
-</div> </center>
-
-
-<center> <div class="container">
-
-<div class="text">
-<p>Increase endurance</p>
-</div>
-
-<div class="round">
-<input type="checkbox" checked id="checkbox4" />
-<label for="checkbox4"></label>
-</div>
-</div> </center>
-
-
-<center> <div class="container">
-
-<div class="text">
-<p>Reduce the risk of injuries</p>
-</div>
-
-<div class="round">
-<input type="checkbox" checked id="checkbox5" />
-<label for="checkbox5"></label>
-</div>
-</div> </center>
-
-
-<center> <div class="container">
-
-<div class="text">
-<p>Adopt a healthy lifestyle</p>
-</div>
-
-<div class="round">
-<input type="checkbox" checked id="checkbox6" />
-<label for="checkbox6"></label>
-</div>
-</div> </center>
-
-<center>
-  <button class="gradient-button" onclick="window.location.href='../physicalactivity/physicalactivity.html';">Continue</button>
-</center> 
-`
-   }
+      this.shadowRoot.innerHTML = `
+          <link rel="stylesheet" type="text/css" href="./goal.css"></head>
+          <img src="../../imgs/left arrow.png">
+          <h1>What is your goal?</h1>
+          <center>
+              <p>Select your fitness goal to personalize your experience in FitZone. Let's get started!</p>
+          </center>
+          ${goalHtml}
+          <center>
+              <button class="gradient-button" onclick="window.location.href='../physicalactivity/physicalactivity.html';">Continue</button>
+          </center>
+      `;
+  }
 }
 
 customElements.define("goal-main", GoalScreen);
